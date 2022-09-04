bb9aadf1abaac588f0e1cbf60db668f3933a3d7d
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-04 07:38:59 +0530

    done

--- study.js
+++ study.js
@@ -1,21 +1,7 @@
-const d1 = new Date(); //  current date time
+const now = new Date(); //  current date time
 
-//  Date objects are mutable
+const newYear = new Date(2022, 0, 1);
 
-d1.setFullYear(2021);
+const newYearIsBefore = newYear < now; //  also: >, >=, <=
 
-d1.setMonth(5); //  June!
-
-d1.setDate(13);
-
-d1.setHours(17);
-
-d1.setMinutes(34);
-
-d1.setSeconds(14);
-
-d1.setMilliseconds(876);
-
-d1.setTime(1662256874407);
-
-//  UTC counterparts are also available
+const diff = now - newYear; //  difference in milliseconds; manually convert it to the units required
